5de9962551121050d1a3da69dd5e723f95a95e0f
LJH's MacBookPro16 <60730765+anshqhsh@users.noreply.github.com>
2021-05-20 22:56:20 +0900

    React 프로젝트전 복습

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') //<div id="root">안에 APP를 등록 해준다.
 );
