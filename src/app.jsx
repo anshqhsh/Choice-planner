a65aeab7a543ecc21636cdbb75fc2eab475f2ccf
LJH's MacBookPro16 <60730765+anshqhsh@users.noreply.github.com>
2021-05-31 16:19:37 +0900

    videodetail ifram을 이용해 데이터 받아 오기

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,18 @@ import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
-
+import VideoDetail from './components/video_detail/video_detail';
 //네트워크 통신을 위한 dependency를 외부에서 받아와야한다 youtube라는 Props로 받아옴
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null); //처음에는 null
+
+  //비디오 선택 콜백함수
+  const selectVideo = video => {
+    setSelectedVideo(video);
+  };
+
   const search = query => {
     youtube
       .search(query) //
@@ -21,7 +28,21 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />;
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+          ;
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from './video_item.module.css';
 
-const VideoItem = ({ video: { snippet } }) => (
-  <li className={styles.container}>
-    <div className={styles.video}>
-      <img
-        className={styles.thumbnails}
-        src={snippet.thumbnails.medium.url}
-        alt="video thumbnail"
-      />
-      <div className={styles.metadata}>
-        <p className={styles.title}>{snippet.title}</p>
-        <p className={styles.channelTitle}>{snippet.channelTitle}</p>
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === 'list' ? styles.list : styles.grid; //디스플레이가 리스트면 스타일에 list를 사용
+  return (
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnails}
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channelTitle}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 export default VideoItem;
